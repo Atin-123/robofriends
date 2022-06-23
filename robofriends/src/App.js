9e032d83cf90ceede5662f16eb285df72b44319d
ATINDRA PAL <atindra8016@gmail.com>
2022-06-24 00:52:50 +0530

    Added onsearchchange functionality

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-	return (
-		<div className="tc">
-			<h1>Robot Friends</h1>
-			<SearchBox/>
-			<CardList robots={robots}/>
-		</div>
-	);
+
+
+class App extends Component {
+	constructor () {
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: ""
+		}
+	}
+	onSearchChange = (event) => {
+		// console.log(event.target.value);
+		this.setState({searchfield: event.target.value})
+	}
+
+	render(){
+		const filteredRobots = this.state.robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		return (
+			<div className="tc">
+				<h1>Robot Friends</h1>
+				<SearchBox searchchange = {this.onSearchChange}/>
+				<CardList robots={filteredRobots}/>
+			</div>
+		);
+	}
 }
 
 export default App;
